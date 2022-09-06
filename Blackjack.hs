7796217fb90b3770917fb3b5038da6d23605a717
Robinsanborn <sanbornr@chalmers.se>
2022-09-06 11:32:58 +0200

    aHand

--- Blackjack.hs
+++ Blackjack.hs
@@ -11,10 +11,10 @@ aCard1 = Card (Numeric 3) Hearts
 aCard2 :: Card
 aCard2 = Card Jack Hearts 
 
-{-
-aHand :: Hand
-aHand = 
 
+aHand :: Hand
+aHand = aCard1 : aCard2 : []
+{-
 
 size hand2
   = size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
